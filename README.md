2a7a7c684a56fa332db527968d43ebd98525f2d2
Salah-Eddine Ismail <pikazeld@gmail.com>
2019-07-08 23:27:44 +0200

    Added line extraction
    Now the program extract all char from the image

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 ##Character Extraction
 Character extraction based on OpenCV and OCR
+
 No machine learning used for extraction here
 
+Still in progress to be more readable
+
 ## Problems
-Not effective with all writing policies
-To many problems, but I wanted to try
-Way better to go to a ML method to find characters in an image as for example a moving frame around the image that would feed a NN which would tell if there's a character in this frame.
+Not effective with all writing policies.
+
+To many problems, but I wanted to try.
+
+Way better to go for a ML method in order to find characters in an image. 
+
+A good method would be for example to move a frame around the image. 
+The content of this frame would then feed a NN which would tell if there's a character or not.
+If there's a character, the content of the frame would be stored to be analyzed by another NN to determine which character it is.
+
--- char_extract.py
+++ char_extract.py
@@ -9,9 +9,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-debug = False
-
-#xor function to detect transition between 0>>1 and 1>>0 to extract char
+##xor function to detect transition between 0>>1 and 1>>0 to extract char
 def xor(a,b):
     return (not(a) and b) or (a and not(b))
 
@@ -23,10 +21,21 @@ def clean_image(img) :
     cleaned_img = cv2.adaptiveThreshold(cleaned_img ,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,5)
     return cleaned_img
 
-img = cv2.imread("text_lyons.png")
-img = img[0:45,:]
-# img = img[45:90,:]
+## This function take as input a boolean vector that represent zones where char have been detected (1)
+## And zone where nothing have been detected (0)
+## Then it extracts indexes where a a zone is crossed (0 -> 1 or 1 -> 0)
+## As 0 means blank, and 1 means char, 0->1->0 means we found a char
+## This function is also used to find lines in a document
+def detect_changes(separation):
+    prev_value = 0
+    index_list = []
+    for index,value in enumerate(separation) :
+        if xor(value,prev_value):
+            index_list = np.concatenate((index_list,[index]))
+        prev_value = value
+    return index_list
 
+img = cv2.imread("text_lyons.png")
 
 # convert image to grayscale in order to treat it easily
 # as color are not useful for text
@@ -34,51 +43,69 @@ gray_img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
 #Improve contrast and widen characters
 gray_img = clean_image(gray_img)
 
-# Sum pixels on the x axis in order to detect blank zones
-x_sum_img = np.sum(gray_img,axis=0)/255
+# Sum pixels on the y axis in order to detect blank zones (space between lines)
+y_sum_img = np.sum(gray_img,axis=1)/255
 
-# determine a threshold value for blank zone as some zones are whitter, I move a bit the threshold
-blank_threshold = np.max(x_sum_img)*0.935
+# determine a threshold value for blank zone
+# (as black=0, white=255, blank zone will have a much higher summed value)
+y_blank_threshold = np.max(y_sum_img)*1
 
-#Zones where a char is present have a value of 1, blank zone a value of 0
+# Zones where a char is present have a value of 1, blank zone a value of 0
+y_separation = (y_sum_img < y_blank_threshold).astype(int)
 
-x_separation = (x_sum_img < blank_threshold).astype(int)
+# extract index where char appears
+# we now there's a char if separation = 1, then we look for index where we jump from 0 to 1 then 1 to 0
+y_index_list = detect_changes(y_separation)
 
-#extract index where char appears
-#we now there's a char if separation = 1, then we look for index where we jump from 0 to 1 then 1 to 0
-prev_value = 0
-index_list = []
-for index,value in enumerate(x_separation) :
-    if xor(value,prev_value):
-        index_list = np.concatenate((index_list,[index]))
-    prev_value = value
+number_of_expected_line = int(len(y_index_list)/2)
 
-#list that will contain all extracted char
-extracted_char = []
-number_of_expected_char = int(len(index_list)/2)
+extracted_line = []
 
-#each couple of value [0,1], [2,3], ... from index_list correspond to character
-for index in range(number_of_expected_char):
-    left_bound = int(index_list[index*2])
-    right_bound = int(index_list[index*2+1])
+# This loop extract lines thanks to idexes listed in index_list
+# Each couple of index represent char zone boundaries
+for index in range(number_of_expected_line):
+    upper_bound = int(y_index_list[index*2])
+    lower_bound = int(y_index_list[index*2+1])
     #Expand the boudaries a little bit on the left and right
-    extracted_char.append(gray_img[ : , left_bound -2 :right_bound +2])
+    extracted_line.append(gray_img[upper_bound -2 :lower_bound +2, :])
 
 
+# Now that we have extracted lines, we can apply the same algorithm as above, but for each line
+# hence the aditionnal for loop running through extracted_line
+extracted_char = []
+
+for line_img in extracted_line :
+    x_sum_img = np.sum(line_img,axis=0)/255
+    x_blank_threshold = np.max(x_sum_img)*0.935
+    x_separation = (x_sum_img < x_blank_threshold).astype(int)
+    x_index_list = detect_changes(x_separation)
 
 
-#Trace the image + summed pixel values + separation
+    #list that will contain all extracted char
+    number_of_expected_char = int(len(x_index_list)/2)
+
+    #each couple of value [0,1], [2,3], ... from index_list correspond to character
+    for index in range(number_of_expected_char):
+        left_bound = int(x_index_list[index*2])
+        right_bound = int(x_index_list[index*2+1])
+        #Expand the boudaries a little bit on the left and right
+        extracted_char.append(line_img[ : , left_bound -2 :right_bound +2])
+
+    print(len(extracted_char))
+    all_char = extracted_char
+    #trace all the char in a plot
+columns = 10
+rows = int( math.ceil( np.round( len(all_char)/5)))
+fig = plt.figure(1,figsize=(columns,rows))
+for i in range(len(all_char)):
+    ax = fig.add_subplot(columns,rows,i+1)
+    plt.imshow(all_char[i], cmap = plt.get_cmap('gray'))
+
+
+# Trace the image + summed pixel values + x_separation
 fig = plt.figure(0)
 ax = fig.add_subplot(111)
 ax.plot(x_sum_img)
-plt.imshow(gray_img,cmap = plt.get_cmap('gray'))
-ax.plot(x_separation)
-
-#trace all the char in a plot
-columns = 5
-rows = int( math.ceil( np.round( len(extracted_char)/5)))
-fig = plt.figure(1,figsize=(columns,rows))
-for i in range(len(extracted_char)):
-    ax = fig.add_subplot(columns,rows,i+1)
-    plt.imshow(extracted_char[i], cmap = plt.get_cmap('gray'))
+plt.imshow(line_img,cmap = plt.get_cmap('gray'))
+ax.plot((x_separation*25))
 plt.show()
