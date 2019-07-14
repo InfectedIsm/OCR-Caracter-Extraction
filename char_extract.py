d1c2e5fd4177428ef42bbb61e36f7ef37f589f03
Salah-Eddine Ismail <pikazeld@gmail.com>
2019-07-14 18:28:48 +0200

    1. Reorganized the code by creating def for each extraction functionnality 2. Added a dictionary to store all character images in an ordered way. Each line have a key indicating its position in the text, same for words into lines, and char into words

--- char_extract.py
+++ char_extract.py
@@ -38,11 +38,11 @@ def detect_changes(separation):
 def trace_all_char(char_list, num_col = 10) :
     # trace all the char in a plot
     num_col = 10
-    rows = int( math.ceil( np.round( len(all_char)/5)))
+    rows = int( math.ceil( np.round( len(char_list)/5)))
     fig = plt.figure(1,figsize=(num_col,rows))
-    for i in range(len(all_char)):
+    for i in range(len(char_list)):
         ax = fig.add_subplot(num_col,rows,i+1)
-        plt.imshow(all_char[i], cmap = plt.get_cmap('gray'))
+        plt.imshow(char_list[i], cmap = plt.get_cmap('gray'))
 
     # Trace the image + summed pixel values + x_separation
     # fig = plt.figure(0)
@@ -52,52 +52,37 @@ def trace_all_char(char_list, num_col = 10) :
     # ax.plot((x_separation * 25))
     plt.show()
 
+def extract_doc2lines(gray_img):
+    # Sum pixels on the y axis in order to detect blank zones (space between lines)
+    y_sum_img = np.sum(gray_img,axis=1)/255
 
+    # determine a threshold value for blank zone
+    # (as black=0, white=255, blank zone will have a much higher summed value)
+    y_blank_threshold = np.max(y_sum_img)*1
 
-img = cv2.imread("text_lyons.png")
+    # Zones where a char is present have a value of 1, blank zone a value of 0
+    y_separation = (y_sum_img < y_blank_threshold).astype(int)
 
-# convert image to grayscale in order to treat it easily
-# as color are not useful for text
-gray_img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-#Improve contrast and widen characters
-gray_img = clean_image(gray_img)
+    # extract index where char appears
+    # we now there's a char if separation = 1, then we look for index where we jump from 0 to 1 then 1 to 0
+    y_index_list = detect_changes(y_separation)
+    number_of_expected_line = int(len(y_index_list)/2)
 
-# Sum pixels on the y axis in order to detect blank zones (space between lines)
-y_sum_img = np.sum(gray_img,axis=1)/255
+    # This loop extract lines thanks to idexes listed in index_list
+    # Each couple of index represent char zone boundaries
+    extracted_line = []
+    for index in range(number_of_expected_line):
+        upper_bound = int(y_index_list[index*2])
+        lower_bound = int(y_index_list[index*2+1])
+        #Expand the boudaries a little bit on the left and right
+        extracted_line.append(gray_img[upper_bound -2 :lower_bound +2, :])
 
-# determine a threshold value for blank zone
-# (as black=0, white=255, blank zone will have a much higher summed value)
-y_blank_threshold = np.max(y_sum_img)*1
+    return extracted_line
 
-# Zones where a char is present have a value of 1, blank zone a value of 0
-y_separation = (y_sum_img < y_blank_threshold).astype(int)
-
-# extract index where char appears
-# we now there's a char if separation = 1, then we look for index where we jump from 0 to 1 then 1 to 0
-y_index_list = detect_changes(y_separation)
-number_of_expected_line = int(len(y_index_list)/2)
-
-# line_dict = dict()
-# for n in number_of_expected_line:
-#     line_dict.setdefault(n,list())
-
-extracted_line = []
-
-
-# This loop extract lines thanks to idexes listed in index_list
-# Each couple of index represent char zone boundaries
-for index in range(number_of_expected_line):
-    upper_bound = int(y_index_list[index*2])
-    lower_bound = int(y_index_list[index*2+1])
-    #Expand the boudaries a little bit on the left and right
-    extracted_line.append(gray_img[upper_bound -2 :lower_bound +2, :])
-
-# Now that we have extracted lines, we can apply the same algorithm as above, but for each line
-# hence the additional for loop running through extracted_line
-
-extracted_word = []
-
-for line_img in extracted_line:
+def extract_lines2words(lines):
+    # Now that we have extracted lines, we can apply the same algorithm as above, but for each line
+    # hence the additional for loop running through extracted_line
+    extracted_word = []
     #bluring the image horizontally in order to merge char, allowing then to detect words
     line_img_blurred = cv2.blur(line_img, (4, 1))
     line_img_blurred = cv2.blur(line_img_blurred, (4, 1))
@@ -106,6 +91,7 @@ for line_img in extracted_line:
     x_separation = (x_sum_img < x_blank_threshold).astype(int)
     word_index_list = detect_changes(x_separation)
 
+    #create a key for each line
 
     number_of_expected_word = int(len(word_index_list) / 2)
 
@@ -116,19 +102,17 @@ for line_img in extracted_line:
         #extract chars, that's why I extract words from line_img and not line_img_blured
         extracted_word.append(line_img[ : , left_bound -2 :right_bound +2])
 
+    return extracted_word
 
-extracted_char = []
+def extract_words2char(word_img):
+    extracted_char = []
 
-for word_img in extracted_word :
     x_sum_img = np.sum(word_img,axis=0)/255
     x_blank_threshold = np.max(x_sum_img)*0.935
     x_separation = (x_sum_img < x_blank_threshold).astype(int)
     x_index_list = detect_changes(x_separation)
 
-
-    #list that will contain all extracted char
     number_of_expected_char = int(len(x_index_list)/2)
-    print(number_of_expected_char)
 
     #each couple of value [0,1], [2,3], ... from index_list correspond to character
     for index in range(number_of_expected_char):
@@ -137,13 +121,43 @@ for word_img in extracted_word :
         #Expand the boudaries a little bit on the left and right
         extracted_char.append(word_img[ : , left_bound -2 :right_bound +2])
 
-    all_char = extracted_char
+    return extracted_char
 
 
-trace_all_char(all_char,10)
+#dictionary that will contain an ordered list of char, sorted by words and lines
+document = {}
+n_lines=0
+n_words=0
+n_char=0
 
-print("lines:", len(extracted_line),
-      "\nwords", len(extracted_word),
-      "\nchar:", len(extracted_char))
+img = cv2.imread("bigger_text.png")
+# convert image to grayscale in order to treat it easily
+# as color are not useful for text
+gray_img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+#Improve contrast and widen characters
+gray_img = clean_image(gray_img)
 
+extracted_line = extract_doc2lines(gray_img)
+
+for line_idx,line_img in enumerate(extracted_line):
+    extracted_word = extract_lines2words(line_img)
+    n_lines+=1
+    document[line_idx] = {}
+
+    for word_idx,word_img in enumerate(extracted_word):
+        n_words += 1
+        extracted_char= extract_words2char(word_img)
+        document[line_idx][word_idx] = {}
+
+        for char_idx, char_img in enumerate(extracted_char):
+            n_char += 1
+            document[line_idx][word_idx][char_idx] = char_img
+
+# print(document)
+
+# trace_all_char(extracted_char,10)
+
+print("lines:", n_lines,
+      "\nwords", n_words,
+      "\nchar:", n_char)
 
